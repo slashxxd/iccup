f57442eef58fdb62457054dd21d920b3cd328b4c
slashxxd <118742856+slashxxd@users.noreply.github.com>
2022-12-19 19:30:36 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,9 +1,11 @@
-import grequests
+import asyncio
+from aiohttp import ClientSession
 
-head = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-"cookie": "threads_read=a%3A23%3A%7Bi%3A1601892%3Bi%3A1671386673%3Bi%3A1603575%3Bi%3A1671386653%3Bi%3A1598491%3Bi%3A1671384293%3Bi%3A1603170%3Bi%3A1671387209%3Bi%3A1603744%3Bi%3A1671387180%3Bi%3A1603685%3Bi%3A1671378844%3Bi%3A1603767%3Bi%3A1671374836%3Bi%3A1603821%3Bi%3A1671374761%3Bi%3A1603372%3Bi%3A1671373652%3Bi%3A1603656%3Bi%3A1671270083%3Bi%3A1602448%3Bi%3A1671270016%3Bi%3A1603436%3Bi%3A1671216113%3Bi%3A1603691%3Bi%3A1671206918%3Bi%3A1603595%3Bi%3A1671206578%3Bi%3A1603501%3Bi%3A1671198086%3Bi%3A1603413%3Bi%3A1671197890%3Bi%3A1603647%3Bi%3A1671174518%3Bi%3A1602469%3Bi%3A1671128893%3Bi%3A1497630%3Bi%3A1671128891%3Bi%3A1603013%3Bi%3A1671113658%3Bi%3A1602442%3Bi%3A1671109508%3Bi%3A1602355%3Bi%3A1671097082%3Bi%3A1603159%3Bi%3A1671097077%3B%7D; forums_read=a%3A3%3A%7Bi%3A1033%3Ba%3A2%3A%7Bs%3A4%3A%22time%22%3Bs%3A10%3A%221671027760%22%3Bs%3A4%3A%22read%22%3Bb%3A1%3B%7Di%3A743%3Ba%3A2%3A%7Bs%3A4%3A%22time%22%3Bs%3A10%3A%221671206621%22%3Bs%3A4%3A%22read%22%3Bb%3A1%3B%7Di%3A701%3Ba%3A2%3A%7Bs%3A4%3A%22time%22%3Bi%3A1670178822%3Bs%3A4%3A%22read%22%3Bb%3A0%3B%7D%7D; media=screen; _ga=GA1.2.268213764.1665660534; _ym_uid=1665660539114445962; _ym_d=1665660539; newlang7=1; _gid=GA1.2.775531604.1671362115; _ym_isad=2; _ym_visorc=b; SID=68t71afna3vdb92aivr7t6bb97; _gat=1; last_session_new=b923254398e32014068ec126f428e6d1; last_session=cdc3a4fa75f27ad6a430a6791c280380",}
+async def hello(url):
+    async with ClientSession() as session:
+        while True:
+            await session.get(url)
 
-response = (grequests.get("https://iccup.com/community/thread/1603767.html", headers=head) for i in range(100))
+loop = asyncio.new_event_loop()
 
-while True:
-    grequests.map(response)
+loop.run_until_complete(hello("https://iccup.com/community/thread/1603767.html"))
